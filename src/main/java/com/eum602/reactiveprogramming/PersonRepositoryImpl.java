d3b002bb59e47cabc74975e9f48bf522f92fda4d
Erick <eum602@gmail.com>
2021-05-15 13:12:42 -0500

    adds simple example how to return a simple element as a data by using mono ('a reactive stream which returns zero or one elements') with the 'Mono.just' method

--- src/main/java/com/eum602/reactiveprogramming/PersonRepositoryImpl.java
+++ src/main/java/com/eum602/reactiveprogramming/PersonRepositoryImpl.java
@@ -5,13 +5,18 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PersonRepositoryImpl implements PersonRepository {
+    Person jhon = new Person(1, "Jhon", "Doe");
+    Person alice = new Person(2, "Alice", "Martins");
+    Person eve = new Person(3, "Eve","Dropper");
+    Person bob = new Person(4, "Bob","Smith");
+
     @Override
     public Mono<Person> getById(Integer id) {
-        return null;
+        return Mono.just(jhon);
     }
 
     @Override
     public Flux<Person> findAll() {
-        return null;
+        return Flux.just(jhon,alice,alice,eve,bob);
     }
 }
