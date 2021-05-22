11bd24e10464a745706e051a096b872369cddf73
Erick <eum602@gmail.com>
2021-05-22 12:12:45 -0500

    reactive: creates a new module with spring initializr with the following packages * Lombok * Spring Reactive Web * Spring data reactive mongoDB * Embedded MongoDB Database

---
+++ netflux/src/main/java/com/eum602/netflux/NetfluxApplication.java
@@ -0,0 +1,13 @@
+package com.eum602.netflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfluxApplication.class, args);
+	}
+
+}
