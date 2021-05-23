d45da2bcd365217499df2d9dfd422ec53eb893ae
Erick <eum602@gmail.com>
2021-05-23 14:08:23 -0500

    configures logging by attaching a netty object into te webClient configuration

--- webclientapp/src/main/java/com/eum602/webclientapp/config/WebClientConfig.java
+++ webclientapp/src/main/java/com/eum602/webclientapp/config/WebClientConfig.java
@@ -1,13 +1,28 @@
 package com.eum602.webclientapp.config;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class WebClientConfig {
     @Bean
     public WebClient webClient(){
-        return WebClient.builder().baseUrl(WebClientProperties.BASE_URL).build();
+        return WebClient.builder()
+                .baseUrl(WebClientProperties.BASE_URL)
+                .clientConnector(
+                        new ReactorClientHttpConnector(
+                        HttpClient
+                                .create() //builds a netty object
+                                .wiretap( //and attaches a configuration for logging
+                                        "reactor.netty.client.httpClient",
+                                        LogLevel.DEBUG,
+                                        AdvancedByteBufFormat.TEXTUAL) //change us the output of the log
+                ))
+                .build();
     }
 }
